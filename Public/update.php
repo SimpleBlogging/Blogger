bb96752b1f73c14ca7dbd57a56582a8114aff202
Ehud Adler <easports96@gmail.com>
2018-08-21 14:19:11 -0400

    Download from dropbox

--- Public/update.php
+++ Public/update.php
@@ -1,9 +1,19 @@
 <?php
-    $post_file = file_get_contents("https://gist.githubusercontent.com/Huddie/29ea20537cb749a85f7a5b49f5d292ea/raw/17088ecdb5b7bf80ce3072af0bebde5249146cdc/post.php");
+    echo "Updating Started\n";
+    echo "Updating poster.php ...\n";
+    
+    $post_file = file_get_contents("https://uc8036df8da624eb0c32a93fb2ad.dl.dropboxusercontent.com/cd/0/get/AOj-_d2xl-zVZ830ksdGYv7fuVsdgXDwI9vXT4ekDkjiHWzmILiIvQ6ZFp-pOPlQyZrYXT9nNBQp1rVlynOB4frqULy0pmkXbEIUYDlzlE9jgxf11TQo-1-sYNAetm77gFdeLMMYx7PnrOyIsmVtizXRsLrpCVFacV9ZUdxTyW_prwRftHytFjMBYXTvu7C0i0g/file?dl=1");
     file_put_contents("poster.php", $post_file);
     
-    $new_file = file_get_contents("https://gist.githubusercontent.com/Huddie/030463513fc9429b6428773fe5519a27/raw/32dba37e80768de334c0ccb8ec754f089484b88d/newBlog.php");
+    echo "poster.php updated\n";
+    echo "Updating new.php ...\n";
+    
+    $new_file = file_get_contents("https://uc34f0470757ebe57c9338896593.dl.dropboxusercontent.com/cd/0/get/AOgUY5XBMe3CrdbEVCo4ZFP7eJPCzfH7smalk0W6sGTLqRGZFb0c-12GKeah5k2DpPd-PCuoVFgo0zfQNjp2SxUgDzhEdV9cxtdybAH9EpXnRSFnZuwQDe-MB1YCUk_8_VGZOop7yiaqVWBNI9a2NacM9TBmI55YM554BpiAudgDPOCvtVrIXUQ6wxyVe52mmTk/file?dl=1");
     file_put_contents("new.php", $new_file);
+    echo "new.php updated\n";
+    
+    echo "Update complete!\n";
 ?>
+
 
 
